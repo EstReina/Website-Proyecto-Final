98022daf5f3ef618410ebbcc74f0877b348b133e
EstReina <estebanreinag@gmail.com>
2024-10-08 00:08:35 -0500

    Update 8-Octubre

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const countrySelect = document.getElementById('pais');
+const cityContainer = document.getElementById('city-container');
+const otherCountryContainer = document.getElementById('other-country-container');
+
+countrySelect.addEventListener('change', function() {
+    const selectedCountry = countrySelect.value;
+
+    // Mostrar ciudades si el país es Colombia
+    if (selectedCountry === 'Colombia') {
+        cityContainer.classList.remove('hidden');
+        otherCountryContainer.classList.add('hidden'); // Oculta el campo de otro país
+    } 
+    // Mostrar campo de texto si el país es Otro
+    else if (selectedCountry === 'Otro') {
+        otherCountryContainer.classList.remove('hidden');
+        cityContainer.classList.add('hidden'); // Oculta la selección de ciudades
+    } 
+    // Si selecciona cualquier otro país
+    else {
+        cityContainer.classList.add('hidden');
+        otherCountryContainer.classList.add('hidden');
+    }
+});
+
+
+//Delvolverse al inicio de la página
+
+document.getElementById('inicio').addEventListener('click', function(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Desplazamiento suave
+    });
+});
